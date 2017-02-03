d1fa03d6b194ea4765f24b936357f8b99d6f6a7c
dvanharl <dwj1122@hotmail.com>
2017-02-03 00:59:40 -0500

    as

--- SSS/js/main.js
+++ SSS/js/main.js
@@ -30,7 +30,7 @@ window.onload = function() {
 		crshr.x = game.input.mousePointer.x;
 		crshr.y = game.input.mousePointer.y
 		if(game.input.activePointer.leftButton.isDown){
-			crshr.animations.play('fire', 30, false);
+			crshr.animations.play('fire', 60, false);
 		}
     }
 };
