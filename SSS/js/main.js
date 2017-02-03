efe2c3f2cda7e450030901cead4db26ff094c749
dvanharl <dwj1122@hotmail.com>
2017-02-03 00:47:03 -0500

    f

--- SSS/js/main.js
+++ SSS/js/main.js
@@ -5,17 +5,18 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-		game.load.image('cursor','assets/Fire.png', 25, 25, 8);
+		game.load.spritesheet('cursor','assets/Fire.png', 25, 25, 8);
     }
     
     var crshr;
+	
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
 		crshr = game.add.sprite(game.world.centerX, game.world.centerY, 'cursor');
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         crshr.anchor.setTo( 0.5, 0.5 );
-        //crshr.animations.add('fire');
+        var fire = crshr.animations.add('fire');
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
@@ -28,8 +29,8 @@ window.onload = function() {
 		//Maintain cursor position on mouse
 		crshr.x = game.input.mousePointer.x;
 		crshr.y = game.input.mousePointer.y
-		//if(game.input.activePointer.leftButton.isDown){
+		if(game.input.activePointer.leftButton.isDown){
 			crshr.animations.play('fire', 30, false);
-		//}
+		}
     }
 };
