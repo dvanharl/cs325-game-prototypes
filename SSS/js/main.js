4a2462e6bc9849a29b92e757962434e755601881
dvanharl <dwj1122@hotmail.com>
2017-02-03 00:31:49 -0500

    Testing Cursor Animation

--- SSS/js/main.js
+++ SSS/js/main.js
@@ -1,23 +1,11 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
+window.onload = function() {  
     "use strict";
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
         // Load an image and call it 'logo'.
-		game.load.image('cursor','assets/Cursor.png');
+		game.load.image('cursor','assets/Fire.png');
     }
     
     var crshr;
@@ -36,14 +24,15 @@ window.onload = function() {
         var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
-    
+	
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
+		//Maintain cursor position on mouse
 		crshr.x = game.input.mousePointer.x;
 		crshr.y = game.input.mousePointer.y
+		game.input.onDown.add(fire,this);
     }
+	
+	function fire(){
+		crshr.animations.play('cursor'),30,false;
+	}
 };
