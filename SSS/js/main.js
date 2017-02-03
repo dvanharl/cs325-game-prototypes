826c9befb95bba4ee4bca7c8993b1ba2fbf61155
dvanharl <dwj1122@hotmail.com>
2017-02-03 02:11:58 -0500

    asdf

--- SSS/js/main.js
+++ SSS/js/main.js
@@ -22,7 +22,7 @@ window.onload = function() {
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing4.", style );
+        var text = game.add.text( game.world.centerX, 15, "Build something amazing5.", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
 	
@@ -30,6 +30,7 @@ window.onload = function() {
 		//Maintain cursor position on mouse
 		crshr.x = game.input.mousePointer.x;
 		crshr.y = game.input.mousePointer.y
+		//If mouse is clicked, play cursor animation to display firing
 		if(game.input.mousePointer.isDown/* && !crashr.animations('fire').isPlaying()*/ && canFire){
 			canFire = false;
 			crshr.animations.play('fire', 30, false);
