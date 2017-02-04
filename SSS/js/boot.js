ed490f878c515c1f64bfb79f4cd2abfad96cb4f9
dvanharl <dwj1122@hotmail.com>
2017-02-04 01:50:22 -0500

    Boot/Gameover Edits

--- SSS/js/boot.js
+++ SSS/js/boot.js
@@ -3,6 +3,9 @@ var boot = function(game){};
 boot.prototype = {
 	preload: function(){},
 	create: function(){
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.setScreenSize();
 		this.game.state.start("Preload");
 	}
 }
--- SSS/js/gameover.js
+++ SSS/js/gameover.js
@@ -2,7 +2,11 @@ var GameOver = function(game){}
 
 gameOver.prototype = {
 	init: function(score){},
-	create: function(){},
+	create: function(){
+		
+		var play = this.game.add.button(160,320,"Get back on the saddle!",this.playGame,this);
+		play.anchor.setTo(0.5,0.5);
+	},
 	playGame: function(){
-	this.game.state.start("Main")
+		this.game.state.start("Game")
 	};
