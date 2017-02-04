341b50fd891172d33c8ec6dccb9078796ab5a5b1
dvanharl <dwj1122@hotmail.com>
2017-02-04 01:52:15 -0500

    f

--- SSS/js/menu.js
+++ SSS/js/menu.js
@@ -2,7 +2,7 @@ var menu = function(game){}
 
 menu.prototype{
 	create: function(){
-		var play = this.game.add.button(160,320,"Load 'em Up!",this.playGame,this);
+		var play = this.game.add.button(400,500,"Load 'em Up!",this.playGame,this);
 		play.anchor.setTo(0.5,0.5);
 	},
 	playgame: function(){
