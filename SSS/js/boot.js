2a6698ea278147f5c2210aef057bae63e3ce6cc4
dvanharl <dwj1122@hotmail.com>
2017-02-04 13:36:05 -0500

    cope

--- SSS/js/boot.js
+++ SSS/js/boot.js
@@ -1,6 +1,18 @@
-var boot = function(game){};
+/*var boot = {};
 
 boot.prototype = {
+	preload: function(){
+		this.game.load.image("backgroudn","assets/background.jpg");
+	},
+	create: function(){
+		//this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		//this.scale.pageAlignHorizontally = true;
+		//this.scale.setScreenSize();
+		this.game.state.start("preload");
+	}
+};*/
+
+var boot = {
 	preload: function(){
 		this.game.load.image("backgroudn","assets/background.jpg");
 	},
--- SSS/js/preload.js
+++ SSS/js/preload.js
@@ -1,6 +1,18 @@
-var preload = function(game){};
+/*var preload = function(game){};
 
 preload.prototype = {
+	preload: function(){
+		var loadBar = this.add.sprite(400,300,"cursor");
+		loadBar.anchor.setTo(0.5,0.5);
+		this.load.setPreloadSprite(loadBar);
+		this.game.load.spritesheet('cursor','assets/Fire.png', 25, 25, 9);
+	},
+	create:function(){
+		this.game.state.start("menu");
+	}
+};*/
+
+var preload = {
 	preload: function(){
 		var loadBar = this.add.sprite(400,300,"cursor");
 		loadBar.anchor.setTo(0.5,0.5);
