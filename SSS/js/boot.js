9b551d68f251dc2b8cbb7dea91185850b46697b2
dvanharl <dwj1122@hotmail.com>
2017-02-04 03:05:07 -0500

    ugh

--- SSS/js/boot.js
+++ SSS/js/boot.js
@@ -3,11 +3,12 @@ var boot = function(game){};
 boot.prototype = {
 	preload: function(){
 		this.game.load.image("cursor","assets/Cursor.png");
+		this.game.load.image("backgroudn","assets/background.jpg");
 	},
 	create: function(){
 		//this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		//this.scale.pageAlignHorizontally = true;
 		//this.scale.setScreenSize();
-		//this.game.state.start("preload");
+		this.game.state.start("preload");
 	}
 };
--- SSS/js/menu.js
+++ SSS/js/menu.js
@@ -2,6 +2,7 @@ var menu = function(game){};
 
 menu.prototype = {
 	create: function(){
+		var bg = game.add.sprite(0,0,"background");
 		var text = game.add.text( game.world.centerX, 15, "OPEN THE GAME" , style );
 		var play = this.game.add.button(400,300,"Load 'em Up!",this.playGame,this);
 		play.anchor.setTo(0.5,0.5);
