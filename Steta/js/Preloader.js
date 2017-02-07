962ee72063895376793b803b7541fe94c15d32bb
dvanharl <dwj1122@hotmail.com>
2017-02-07 01:04:28 -0500

    f

--- Steta/js/Preloader.js
+++ Steta/js/Preloader.js
@@ -26,7 +26,7 @@ BasicGame.Preloader.prototype = {
 		//	As this is just a Project Template I've not provided these assets, swap them for your own.
 		this.load.image('titlePage', 'assets/title.jpg');
 		this.load.atlas('playButton', 'assets/play_button.png', 'assets/play_button.json');
-		this.load.audio('titleMusic', ['assets/Poppers and Prosecco.mp3']);
+		//this.load.audio('titleMusic', ['assets/Poppers and Prosecco.mp3']);
 		//	+ lots of other required assets here
 		this.load.spritesheet('cursor','assets/Fire.png',25, 25, 9);
 		this.load.image('meteor','assets/ast.jpeg');
