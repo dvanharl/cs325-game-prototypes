334f71d17a77e7cfb243fa297f83452d2bb1d28f
dvanharl <dwj1122@hotmail.com>
2017-02-07 17:39:35 -0500

    f

--- Steta/js/MainMenu.js
+++ Steta/js/MainMenu.js
@@ -21,18 +21,12 @@ BasicGame.MainMenu.prototype = {
 
 	update: function () {
 
-		//	Do some nice funky main menu effect here
 
 	},
 
 	startGame: function (pointer) {
-
-		//	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
 		this.music.stop();
-
-		//	And start the actual game
 		this.state.start('Game');
-
 	}
 
 };
