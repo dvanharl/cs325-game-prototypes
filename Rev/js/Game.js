20d8a37581fb7dd33e45f850dcafe007d02f1711
dvanharl <dwj1122@hotmail.com>
2017-02-16 06:30:45 -0500

    f

--- Rev/js/Game.js
+++ Rev/js/Game.js
@@ -313,10 +313,8 @@ BasicGame.Game.prototype = {
 			//Bullet Collision
 			if((this.bullet.x < this.enemies.children[i].x + 30 && this.bullet.x > this.enemies.children[i].x - 30) && (this.bullet.y < this.enemies.children[i].y + 60 && this.bullet.y > this.enemies.children[i].y - 60) && this.bullet.alive){
 				this.enemies.children[i].tint = 0xff0000;
-				this.add.tween(this.enemies.children[i]).to({alpha:0}, 300, Phaser.Easing.Linear.None, true, 0,0,false);
-				this.time.events.add(300,function(){
-					this.invincible = false;
-					this.player.tint = 0xffffff;
+				this.add.tween(this.enemies.children[i]).to({alpha:0}, 100, Phaser.Easing.Linear.None, true, 0,0,false);
+				this.time.events.add(100,function(){
 					this.dead = this.enemies.remove(this.enemies.children[i], true);
 					this.die.play();
 					this.bullet.kill();
