3c487144e3d574a43b3734ffa7bf46bc3b3558da
dvanharl <dwj1122@hotmail.com>
2017-02-15 20:18:21 -0500

    f

--- Rev/js/MainMenu.js
+++ Rev/js/MainMenu.js
@@ -17,7 +17,8 @@ BasicGame.MainMenu.prototype = {
 		this.music.play();
 
 		this.playButton = this.add.button( 303, 400, 'playButton', this.startGame, this, 'over', 'out', 'down');
-
+		this.blackScreen = this.add.sprite(0,0,'blackScreen');
+		this.blackScreen.alpha = 0;
 	},
 
 	update: function () {
@@ -26,8 +27,7 @@ BasicGame.MainMenu.prototype = {
 	},
 
 	startGame: function (pointer) {
-		this.blackScreen = this.add.sprite(0,0,'blackScreen');
-		this.blackScreen.alpha = 0;
+		
 		this.music.fadeOut(2000);
 		this.add.tween(this.blackScreen).to({alpha:1}, 1500, Phaser.Easing.Linear.None, true, 1500,0,false);
 		this.music.onFadeComplete.add(function() {
