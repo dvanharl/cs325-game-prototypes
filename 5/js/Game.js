cc3ad7b11a09098a7c8da38c7f18cc3c2fb78a00
dvanharl <dwj1122@hotmail.com>
2017-03-07 22:05:19 -0500

    f

--- 5/js/Game.js
+++ 5/js/Game.js
@@ -38,12 +38,16 @@ BasicGame.Game = function (game) {
 
 BasicGame.Game.prototype = {
     create: function () {
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+		
 		//Set up map
 		this.map = this.add.tilemap('stage1');
 		this.map.addTilesetImage('floortile', 'tiles');
 		this.floor = this.map.createLayer('Floor');
-		this.Walls = this.map.createLayer('Walls');
+		this.wall = this.map.createLayer('Walls');
 		this.map.setCollisionBetween(1, 300, true, 'Walls');
+		//this.physics.p2.convertTilemap(this.map, this.wall);
+		
 		
 		//Set up Player
 		this.player = this.add.sprite(450, 570, 'player');
@@ -58,7 +62,7 @@ BasicGame.Game.prototype = {
     update: function () {
 		//MOVEMENT
         this.updateMove();
-		
+		this.physics.arcade.collide(this.player, this.wall);
     },
 	
 	render: function() {
