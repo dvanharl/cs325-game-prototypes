da6ff78eeb848124b694e68e4dc152926de99e50
dvanharl <dwj1122@hotmail.com>
2017-03-08 00:19:45 -0500

    f

--- 5/js/Stage2.js
+++ 5/js/Stage2.js
@@ -70,7 +70,7 @@ BasicGame.Stage2.prototype = {
 		
 		this.arrows = this.input.keyboard.createCursorKeys();
 		
-		this.map.replace(3,5);
+		this.map.replace(3,5,1,300,20,15,'Floor');
     },
 
     update: function () {
