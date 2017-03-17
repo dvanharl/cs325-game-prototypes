50f19a8de1216d296d421cdf69f1ee2203eda990
dvanharl <dwj1122@hotmail.com>
2017-03-17 19:13:30 -0400

    f

--- Bonus/js/MainMenu.js
+++ Bonus/js/MainMenu.js
@@ -32,7 +32,7 @@ BasicGame.MainMenu.prototype = {
 		this.controls = this.add.sprite(325, 480, 'controls');
 		
 		this.cursor = this.add.sprite(250, 400, 'cursor');
-		this.cursor.animations.add('idle',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], 30, true, true);
+		this.cursor.animations.add('idle',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21], 30, true, true);
 		this.cursor.play('idle');
 		
 		this.sel = -1;
