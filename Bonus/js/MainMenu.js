7f607c323b913de3b862f981a937b280101af40b
dvanharl <dwj1122@hotmail.com>
2017-03-17 18:01:45 -0400

    f

---
+++ Bonus/js/MainMenu.js
@@ -0,0 +1,83 @@
+window.addEventListener("keydown", function(e){
+	if([32,37,38,39,40].indexOf(e.keyCode) > -1) {
+		e.preventDefault();
+	}
+},false);
+
+BasicGame.MainMenu = function (game) {
+
+	this.music = null;
+	this.play = null;
+	this.controls = null;
+	this.sel = null;
+	this.cursor = null;
+	this.opCon = null;
+
+};
+
+BasicGame.MainMenu.prototype = {
+
+	create: function () {
+
+		//	We've already preloaded our assets, so let's kick right into the Main Menu itself.
+		//	Here all we're doing is playing some music and adding a picture and button
+		//	Naturally I expect you to do something significantly better :)
+
+		this.music = this.add.audio('titleMusic');
+		this.music.play();
+
+		this.add.sprite(0, 0, 'titlePage');
+
+		this.play = this.add.sprite(325, 400, 'play');
+		this.controls = this.add.sprite(325, 480, 'controls');
+		
+		this.cursor = this.add.sprite(250, 400, 'cursor');
+		this.cursor.animations.add('idle',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], 30, true, true);
+		this.cursor.play('idle');
+		
+		this.sel = -1;
+		this.opCon = false;
+		this.controls.kill();
+		
+		this.whiteScreen = this.add.sprite(0,0,'whiteScreen');
+		this.add.tween(this.whiteScreen).to({alpha:0}, 2000, Phaser.Easing.Linear.None, true, 0,0,false);
+	},
+
+	update: function () {
+		if(!this.opCon){
+			if(this.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+				this.sel = this.sel * -1;
+			}else if(this.input.keyboard.isDown(Phaser.Keyboard.UP)){
+				this.sel = this.sel * -1;
+			}
+			
+			this.cursor.y = 440 + (40 * this.sel);
+
+			if(this.input.keyboard.isDown(Phaser.Keyboard.Z)){
+				if(this.sel == -1){
+					this.startGame();
+				}else{
+					this.openControls();
+				}
+			}
+		}else{
+			if(this.input.keyboard.isDown(Phaser.Keyboard.X)){
+				this.controls.kill();
+				this.opCon = false;
+			}
+		}
+	},
+
+	startGame: function (pointer) {
+		this.add.tween(this.whiteScreen).to({alpha:1}, 1500, Phaser.Easing.Linear.None, true, 0,0,false);
+		this.music.fadeOut(2000);
+		this.music.onFadeComplete.add(function() {
+			this.state.start('Stage1');
+		},this);
+	},
+	
+	openControls: function (){
+		this.controls.revive();
+		this.opCon = true;
+	}
+};
---
+++ Bonus/js/Stage1.js
@@ -0,0 +1,38 @@
+window.addEventListener("keydown", function(e){
+	if([32,37,38,39,40].indexOf(e.keyCode) > -1) {
+		e.preventDefault();
+	}
+},false);
+
+BasicGame.Stage1 = function (game) {
+
+    //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+
+    this.game;      //  a reference to the currently running game (Phaser.Game)
+    this.add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
+    this.camera;    //  a reference to the game camera (Phaser.Camera)
+    this.cache;     //  the game cache (Phaser.Cache)
+    this.input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
+    this.load;      //  for preloading assets (Phaser.Loader)
+    this.math;      //  lots of useful common math operations (Phaser.Math)
+    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
+    this.stage;     //  the game stage (Phaser.Stage)
+    this.time;      //  the clock (Phaser.Time)
+    this.tweens;    //  the tween manager (Phaser.TweenManager)
+    this.state;     //  the state manager (Phaser.StateManager)
+    this.world;     //  the game world (Phaser.World)
+    this.particles; //  the particle manager (Phaser.Particles)
+    this.physics;   //  the physics manager (Phaser.Physics)
+    this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
+
+};
+
+BasicGame.Stage1.prototype = {
+
+    create: function () {
+    },
+
+    update: function () {
+    }
+
+};
